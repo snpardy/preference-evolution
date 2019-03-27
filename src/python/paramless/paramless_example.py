1fa9541ba45f439e61b88b86ca5e7f5387b641a5
Sam Pardy <snpardy@gmail.com>
2019-03-27 16:55:23 +1100

    More work on proposal and cleaned python code

--- src/python/paramless/paramless_example.py
+++ src/python/paramless/paramless_example.py
@@ -6,46 +6,50 @@ from copy import deepcopy as deepcopy
 import math
 import paramless_two_args as p
 
+
 def my_multivariate_normal(x, y, mutation_epsilon, mean_x, mean_y, variance):
     # This function assumes that the pearson correlation coefficient is 0
     return mutation_epsilon * (math.e ** -((.5)*((((x-mean_x)**2)/variance**2) + (((y-mean_y)**2)/variance**2))))
-    
+
+
 def example_evolution():
     # the x and y axes
+    # x is my pay-off and y is my opponent - so, we begin with Z=X
     x = np.arange(0, 10, dtype=float)
     y = np.arange(0, 10, dtype=float)
     
-    #Create grid and multivariate normal
+    # Create grid and multivariate normal
     X, Y = np.meshgrid(x,y)
     Z = my_multivariate_normal(X, Y, 1, 5, 5, 3)
 
-
     target_surface = [X,Y,Z]
-    initial_surface = [X,Y,np.zeros_like(X, dtype=float)]
-    result_surface, time_series = p.evolve(initial_surface, p.distance_fitness_function, p.gaussian_mutation, 10, target_surface=target_surface,
-    mutation_epsilon=.05, time_series_data=True, radius=4)
-    print("Evolution is done, now creating a GIF. This can take a while...")
-    p.createGIF("src\python\paramless", time_series)
-    print("Done!")
+    initial_surface = [X,Y, (deepcopy(X))/10]
+    # result_surface, time_series = p.evolve(initial_surface, p.distance_fitness_function, p.gaussian_mutation, 1000, target_surface=target_surface,
+    # mutation_epsilon=.05, time_series_data=True, radius=4)
+    
+    # print("Evolution is done, now creating a GIF. This can take a while...")
+    # p.createGIF("src\python\paramless", time_series)
+    # print("Done!")
    
-    #Make a 3D plot
+    # Make a 3D plot
     fig = plt.figure()
     ax1 = fig.add_subplot(121, projection='3d')
     ax1.set_zlim3d(0, 1.1)
-    ax1.plot_surface(X, Y, Z,cmap='viridis',linewidth=0)
-    ax1.set_title('Target Surface')
+    ax1.plot_surface(X, Y, target_surface[2],cmap='viridis',linewidth=0)
+    ax1.set_title('Initial Surface')
     ax1.set_xlabel('X axis')
     ax1.set_ylabel('Y axis')
     ax1.set_zlabel('Z axis')
     
     ax2 = fig.add_subplot(122, projection='3d')
     ax2.set_zlim3d(0, 1.1)
-    ax2.plot_surface(X, Y, result_surface[2],cmap='viridis',linewidth=0)
+    ax2.plot_surface(X, Y, initial_surface[2],cmap='viridis',linewidth=0)
     ax2.set_title('Evolved Surface')
     ax2.set_xlabel('X axis')
     ax2.set_ylabel('Y axis')
     ax2.set_zlabel('Z axis')
     plt.show()
+
 
 if __name__ == '__main__':
     example_evolution()
--- src/python/paramless/paramless_two_args.py
+++ src/python/paramless/paramless_two_args.py
@@ -1,18 +1,15 @@
 
-'''
+"""
 Created on 2019-03-05 12:59
 @summary: Paramless evolution on a 3D surface. Based on Julian Garcia's paramless.py
 @author: snpardy
-'''
+"""
 
 import numpy as np
 import math
-from collections import OrderedDict as OrderedDict
 import matplotlib.pyplot as plt
-import matplotlib
 import os
 import imageio
-# from mpl_toolkits.mplot3d import axes3d
 
 # default tolerance for float comparisons
 DEFAULT_ATOL = 1e-8
@@ -20,49 +17,55 @@ DEFAULT_ATOL = 1e-8
 # maximum iterations
 MAX_ITERATIONS = 1000000
 
+
 def _within_bounds(vector, lower_bound, upper_bound):
-    '''
+    """
     @summary: Determines if the values of vector are in the [lower_bound, upper_bound] interval
     @param vector: surface to test
     @param lower_bound: lower_bound
     @param upper_bound: upper_bound
     @result: True or False 
     @todo: Add tolerance!
-    '''
-    if lower_bound != None and np.min(vector) < lower_bound :
+    """
+    if lower_bound is not None and np.min(vector) < lower_bound:
         return False
-    if upper_bound != None and np.max(vector) > upper_bound:
+    if upper_bound is not None and np.max(vector) > upper_bound:
         return False
     return True
 
+
 def _bivariate_normal_mutation(x, y, mutation_epsilon, mean_x, mean_y, variance):
     # This function assumes that the pearson correlation coefficient is 0
-    return mutation_epsilon * (math.e ** -((.5)*((((x-mean_x)**2)/variance**2) + (((y-mean_y)**2)/variance**2))))
+    return mutation_epsilon * (math.e ** -(.5*((((x-mean_x)**2)/variance**2) + (((y-mean_y)**2)/variance**2))))
 
-def _attemp_gaussian_mutation(X,Y,Z, mutation_epsilon, radius, lower_bound=None, upper_bound=None):
+
+def _attempt_gaussian_mutation(X, Y, Z, mutation_epsilon, radius, lower_bound=None, upper_bound=None):
     location = np.array([np.random.randint(0, len(X)), np.random.randint(0, len(Y))])
     mutant = np.copy(Z)
     variance = np.random.rand() * radius
     perturbation = _bivariate_normal_mutation(X, Y, mutation_epsilon, location[0], location[1], variance)
     # upwards
-    if (np.random.randint(2)):
+    if np.random.randint(2):
         mutant += perturbation
     # downwards
     else:
         mutant -= perturbation
     return mutant
 
+
 def gaussian_mutation(X,Y,Z, mutation_epsilon, radius, lower_bound=None, upper_bound=None, **kwargs):
     is_inside = False
-    attemp = 0
-    while (not is_inside):
-        mutant = _attemp_gaussian_mutation(X,Y,Z, mutation_epsilon, radius, lower_bound=lower_bound, upper_bound=upper_bound)
-        is_inside=_within_bounds(mutant, lower_bound, upper_bound)
-        attemp+=1
-        if attemp > MAX_ITERATIONS:
+    attempt = 0
+    mutant = None
+    while not is_inside:
+        mutant = _attempt_gaussian_mutation(X, Y, Z, mutation_epsilon, radius, lower_bound=lower_bound, upper_bound=upper_bound)
+        is_inside = _within_bounds(mutant, lower_bound, upper_bound)
+        attempt += 1
+        if attempt > MAX_ITERATIONS:
             raise RuntimeError("Attempted too many mutations without producing anythin within bounds")
             pass
     return mutant
+
 
 def _surface_distance(u, v):
     """
@@ -80,9 +83,10 @@ def _surface_distance(u, v):
 
     return total
 
+
 def distance_fitness_function(resident, mutant, target_surface, **kwargs):
     """
-    Returns the fitness of surfaces of two paramaters.
+    Returns the fitness of surfaces of two parameters.
     @returns fitness_resident, fitness_mutant
     @param resident: the resident surface, of which the fitness will be returned
     @param mutant: the mutant surface, of which the fitness will be returned
@@ -92,28 +96,31 @@ def distance_fitness_function(resident, mutant, target_surface, **kwargs):
     fitness_mutant = 1.0 / _surface_distance(mutant, target_surface)
     return fitness_resident, fitness_mutant
 
+
 def evolution_step(resident_surface, fitness_function, mutation_function, atol, *args, **kwargs):
     """
     One generation iteration
     """
     invasion = False
-    mutantVector = mutation_function(resident_surface[0], resident_surface[1], resident_surface[2], **kwargs)
-    fitness_resident, fitness_mutant = fitness_function(resident_surface, [resident_surface[0], resident_surface[1], mutantVector], **kwargs)
+    mutant_vector = mutation_function(resident_surface[0], resident_surface[1],
+                                      resident_surface[2], **kwargs)
+    fitness_resident, fitness_mutant = fitness_function(resident_surface,
+                                                        [resident_surface[0],
+                                                         resident_surface[1], mutant_vector], **kwargs)
     if fitness_resident < fitness_mutant and abs(fitness_resident - fitness_mutant) > atol:
         resident_surface = np.copy(resident_surface)
-        resident_surface[2] = np.copy(mutantVector)
+        resident_surface[2] = np.copy(mutant_vector)
         invasion = True
     return resident_surface, invasion
 
 
-
-def evolve(initial_surface, fitness_function, mutation_function, iterations, atol=DEFAULT_ATOL, seed=None, time_series_data=False, **kwargs):
+def evolve(initial_surface, fitness_function, mutation_function, iterations,
+           atol=DEFAULT_ATOL, seed=None, time_series_data=False, **kwargs):
     """
     Evolve
     """
     np.random.seed(seed)
     resident = np.copy(initial_surface)
-    previous_resident = np.zeros_like(initial_surface)
     if time_series_data:
         time_series_array = [resident]
     for step in range(1, iterations):
@@ -125,8 +132,9 @@ def evolve(initial_surface, fitness_function, mutation_function, iterations, ato
         return resident, time_series_array
     return resident
 
-def createGIF(path, time_series_array):
-    os.mkdir(path + "\.plots")
+
+def create_gif(path, time_series_array):
+    os.mkdir(path + "\\.plots")
     fig = plt.figure()
     images = []
     for i, item in enumerate(time_series_array):
@@ -136,14 +144,13 @@ def createGIF(path, time_series_array):
         ax.set_xlabel('X axis')
         ax.set_ylabel('Y axis')
         ax.set_zlabel('Z axis')
-        ax.plot_surface(item[0], item[1], item[2],cmap='viridis',linewidth=0)
-        file_name = path + "\.plots\plot_" + str(i) + '.png'
+        ax.plot_surface(item[0], item[1], item[2], cmap='viridis',linewidth=0)
+        file_name = path + "\\.plots\plot_" + str(i) + '.png'
         fig.savefig(file_name)
 
-
     for i in range(len(time_series_array)):
-        fn = path + "\.plots\plot_" + str(i) + '.png'
+        fn = path + "\\.plots\plot_" + str(i) + '.png'
         images.append(imageio.imread(fn))
         os.remove(fn)
-    os.rmdir(path+"\.plots")
-    imageio.mimsave(path +'\evolution.gif', images)
+    os.rmdir(path+"\\.plots")
+    imageio.mimsave(path + '\\evolution.gif', images)
